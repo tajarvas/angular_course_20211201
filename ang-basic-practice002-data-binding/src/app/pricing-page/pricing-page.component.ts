bfb12eb03582028a0e4d254fafa1acd343c74232
Tamás Járvás <tajarvas@molgroup.info>
2021-12-01 15:03:16 +0100

    nexxt

--- ang-basic-practice002-data-binding/src/app/pricing-page/pricing-page.component.ts
+++ ang-basic-practice002-data-binding/src/app/pricing-page/pricing-page.component.ts
@@ -16,9 +16,14 @@ export class PricingPageComponent implements OnInit {
 
   priceTable: IPriceItem[] = [];
 
-  constructor() { }
+  constructor() {  }
 
   ngOnInit(): void {
-  }
+    this.priceTable = [
+      {name: "Free", price: 0, options: ['10 tanuló','2 GB tárhely','Email támogatás','Dokumentáció'], btnText: "Ingyenes regisztráció"},
+      {name: "Pro", price: 15, options: ['20 tanuló','10 GB tárhely','Kiemelt email támogatás','Dokumentáció'], btnText: "Kezdő lépések"},
+      {name: "Enterprise", price: 29, options: ['100 tanuló','100 GB tárhely','telefonos és email támogatás','Dokumentáció'], btnText: "Kontakt"}
+  ];
+}
 
 }
